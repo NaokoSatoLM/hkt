7542af786d555864681cbc1543ed03d791cb4b40
NaokoSatoLM <naoko.sato@lifematics.co.jp>
2020-01-27 09:31:03 +0900

    Add VS extension of code-line-check

--- service/resources/views/shipping/ks_index.blade.php
+++ service/resources/views/shipping/ks_index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,20 +12,21 @@
 
     <!-- Bulma -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-        <meta charset="utf-8">
-        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <meta charset="utf-8">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
     <!-- Styles -->
     <style>
-        html, body {
+        html,
+        body {
             background-color: #fff;
             color: #636b6f;
             font-family: 'Nunito', sans-serif;
             font-weight: 200;
             height: 100vh;
             margin: 0;
-            font-size:40px;
+            font-size: 40px;
         }
 
         .content {
@@ -32,46 +34,56 @@
             margin: 0px;
         }
 
-        .header{
-            padding-top:40px;
-            font-size:60px;
-            height:200px;
+        .header {
+            padding-top: 40px;
+            font-size: 60px;
+            height: 200px;
         }
-        .header-btn{
-            margin-left:93%;
+
+        .header-btn {
+            margin-left: 93%;
         }
+
         .box {
             margin: 0 auto;
             padding-top: 1em;
-            width: 90%; /*幅の調節*/
-            color: #666666; /*文字色*/
-            border: 1px solid #ccc; /*線の太さ・色*/
-            border-radius: 5px; /*角の丸み*/ 
+            width: 90%;
+            /*幅の調節*/
+            color: #666666;
+            /*文字色*/
+            border: 1px solid #ccc;
+            /*線の太さ・色*/
+            border-radius: 5px;
+            /*角の丸み*/
         }
-        .title{
-            margin-top:20px;
+
+        .title {
+            margin-top: 20px;
         }
-        .set-size{
-            height:50px;
-            width:10%;
-            font-size:40px;
+
+        .set-size {
+            height: 50px;
+            width: 10%;
+            font-size: 40px;
         }
-        .table{
-            font-size:40px;
+
+        .table {
+            font-size: 40px;
         }
     </style>
 
 </head>
+
 <body>
     <div class="content">
-    <div>
+        <div>
             <nav class="header is-primary has-text-white  has-background-info">
                 受注内容確認システム
                 <div class="header-btn">
                     <a href="./index">
                         <div class="button">
                             <div class="level-item has-text-centered">
-                                    TOP画面に戻る
+                                TOP画面に戻る
                             </div>
                         </div>
                     </a>
@@ -107,7 +119,7 @@
         <div class="box">
             <!-- 対象がなければエラーメッセージ -->
             @if(isset($msg))
-                {{$msg}}
+            {{$msg}}
             @else
             <div id='statusList'>
                 <table class="table is-striped is-hoverable" border="1">
@@ -127,12 +139,12 @@
                         <td>{{$data['受付日時']}}</td>
                         <td>{{$data['ステータス']}}</td>
                         <td>
-                        <form action="/ks_check" method="GET">
-                            <input type="submit"  value="確認" class="button is-info">
-                            <input type="hidden" name="company" id="company" value="{{$data['発注者']}}">
-                            <input type="hidden" name="place" id="place" value="{{$data['納品先']}}">
-                            <input type="hidden" name="confirm_id" id="confirm_id" value="{{$data['id']}}">
-                        </form>
+                            <form action="/ks_check" method="GET">
+                                <input type="submit" value="確認" class="button is-info">
+                                <input type="hidden" name="company" id="company" value="{{$data['発注者']}}">
+                                <input type="hidden" name="place" id="place" value="{{$data['納品先']}}">
+                                <input type="hidden" name="confirm_id" id="confirm_id" value="{{$data['id']}}">
+                            </form>
                         </td>
                     </tr>
                     @endforeach
